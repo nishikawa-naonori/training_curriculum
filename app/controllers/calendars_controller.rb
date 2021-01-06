1c9b64cbf8aaa42b77c2f2ca8f076b2a3c7b0e08
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-01-06 15:41:31 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,15 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = Date.today.wday + x 
+      puts wday_num
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, :wday => wdays[wday_num]}
+      puts "wdays[wday_num]}の中身は、#{days[:wday]}"
       @week_days.push(days)
     end
 
